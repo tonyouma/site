322405c172b547716ed251dfbf5a949394841291
Anthony Rubua <rubua31@gmail.com>
2022-04-28 13:19:13 +0300

    fixed app layout

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import type { NextPage } from 'next'
-import { Header } from 'src/components/header';
-import { Navbar } from 'src/components/navbar';
-import { Footer } from 'src/components/footer';
+
 
 const clients: NextPage =()=> {
   return (
-    <>
-      <Header />
-      <Navbar />
-
-      <Footer />
-    </>
+   <div>
+     clients
+   </div>
   )
 }
 
