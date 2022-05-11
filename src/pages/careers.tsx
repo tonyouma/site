1b51c0b939b2bf661edd8d528704bbe719083066
TonyOuma <kyrietony@gmail.com>
2022-05-11 10:29:35 +0300

    feat: update careers page contents

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,34 +1,48 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 import CareersCard from 'src/components/careersCard'
+
+// const TitleItem = (title: string) => {
+//   return (
+//     <h3 className='py-4 text-center'>{title}</h3>
+//   )
+// }
 const careers: NextPage = () => {
   return (
-    <div>
-      <div className='md:px-60 px-6 pt-6'>
-        <h3>About Data Integrated Limited (DIL)</h3>
+    <div className=''>
+      <div>
+        <img src="/assets/careers.png" alt="" />
+      </div>
+      <div className='container px-5 md:px-40 pt-11'>
+        <h3 className='pb-6'>About Data Integrated Limited (DIL)</h3>
         <p>Data Integrated Limited (DIL) is a Kenyan ICT company that offers financial solutions to small and medium scale enterprises (SME’s) in Africa. The company humble beginnings dates back to 2012, armed with a big dream of addressing automation challenges faced by small and medium scale businesses (SME’s) in Africa.
           We believe that SME’s would realize their financial goals sooner by deploying modern technology. The efficiency thus gained would translate to growth and longevity of the businesses.</p>
-        <p><span className='font-bold my-4'>Our motto</span>  says a lot on what we believe in, which is connecting “the street” to “high finance”. </p>
+        <p><span className='my-4 font-bold'>Our motto</span>  says a lot on what we believe in, which is connecting “the street” to “high finance”. </p>
         <p>Over the years, DIL has mastered the African market and offers niche solutions that are customized to the unique African experience. We are committed to giving efficient services, seamless customers experience and user friendly person-to-person payments.<br />
           Our solutions are developed to be compatible with most payments solutions and platforms. DIL strives to have financial inclusion for all, by increasing payment efficiency through liable, secure, fast and easy to use devices at an affordable price.</p>
       </div>
 
-      <div className=' my-6 bg-white'>
-        <div className="py-12 md:px-60 px-6">
-          <p className='text-center'>Currently Open Roles</p>
-          <CareersCard />
-          <div className='md:w-2/3 mx-auto text-center'>
-            <p className=''>Open Application</p>
-            <p>If you don’t see an available position but think you can still be a valuable addition to our team, please send in your application to <span className='text-blue-500 font-bold'>hr@dataintegrated.co.ke</span>  and convince us.</p>
-            <p className=''>Internships</p>
+      <div className='py-12 my-6 bg-primary'>
+        <div className="container px-5 md:px-40">
+          <h4 className='text-center'>Currently Open Roles</h4>
+          <div className='flex flex-col py-5 gap-y-5'>
+            <CareersCard />
+            <CareersCard />
+            <CareersCard />
+          </div>
+          <div className='text-center'>
+            <h4 className='pb-3'>Open Application</h4>
+            <p>If you don’t see an available position but think you can still be a valuable addition to our team, please send in your application to <span className='font-bold text-blue-500'>hr@dataintegrated.co.ke</span>  and convince us.</p>
+            <h4 className='py-3'>Internships</h4>
             <p>We always welcome interns with high energy, enthusiasm and a keen willingness to learn from day one. Let us know if you are one of them.</p>
           </div>
         </div>
       </div>
 
-      <div className='md:px-60 px-8 py-8 mx-auto text-center'>
-        <p className=''>Benefits of working for us</p>
-        <p className='pt-6'>In addition to contributing to the great cause of easing the lives of millions of Afri- cans with world-class financial products, you also get competitive base salaries, social and statuatory benefits, generous holidays, commissions / bonus / profit sharing schemes based on responsibilities and results of different positions.</p>
+      <div className='container px-8 py-8 mx-auto text-center md:px-40'>
+        <h4 className=''>Benefits of working for us</h4>
+        <p className='pt-6'>In addition to contributing to the great cause of easing the lives of millions of Africans with world-class financial products, you also get competitive base salaries, social and statuatory benefits, generous holidays, commissions / bonus / profit sharing schemes based on responsibilities and results of different positions.</p>
       </div>
 
 
---
+++ src/pages/products 2.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Link } from "../components/link";
+import { type } from "os";
+
+type feature = {
+    id: string;
+    title: string;
+    icon: string;
+    text: string;
+}
+
+
+
+const products: NextPage = () => {
+
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2">
+          <div className=" mt-24">
+            <h3 className="text-xs font-medium text-slate-500 m-4">
+              YOUR ULTIMATE PASSENGER APP
+            </h3>
+            <h3 className="text-lg font-black m-4">M-EPESI APPLICATION</h3>
+            <p className="font-normal text-slate-400 m-4">
+              A smartphone application that allows users to view and utilize
+              real-time public transit. It simplifies transportation by allowing
+              customers to know which vehicles are available for booking,
+              calculate the fee, and view traffic conditions on the route.
+            </p>
+            <div className="flex mt-10 ml-4">
+              <a>
+                <img
+                  className="inline w-32 mr-4 "
+                  src="/assets/appstore.svg"
+                  alt=""
+                />
+              </a>
+              <Link
+                name="Play Store"
+                blank
+                url="#"
+                image={"/assets/playstore.svg"}
+              />
+            </div>
+          </div>
+          <div className="border-solid border-2 border-red-500 flex justify-center">
+            <img
+              src="/assets/products/grouped.svg"
+              className="absolute w-96"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="h-full bg-gradient-to-r from-transparent via-white mt-24">
+        <div className="container mx-auto">
+          <div className=" mt-28">
+            <h3 className="text-lg font-bold">Why choose M-Epesi?</h3>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default products;
