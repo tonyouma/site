7756aecc2f16f4f469d2d75e799efeb41946dc3c
TonyOuma <kyrietony@gmail.com>
2022-06-06 11:32:19 +0300

    feat: update careers page

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -17,57 +17,57 @@ interface Props {
 const careers = ({ careers }: Props) => {
 
   return (
-    <div className=''>
-      <div className='h-[552px] bg-black relative text-center'>
-        <img src="/assets/careers.png" alt=""  className='object-cover opacity-70'/>
-        <h1 className='careers-intro'>Careers at DIL</h1>
+    <div className='container'>
 
-        
-      </div>
-      <div className='container px-5 md:px-40 pt-11'>
+      <h1 className='mb-4 text-center'>A meaningful challenge to solve. A <br /> soulful company to work for.</h1>
+
+      <p className='max-w-2xl py-8 mx-auto text-center'>
+        “I never lose. I either win or learn”, says an African proverb. We go all-in when we try to come up with products that make a common man’s life easy. And we do that by standing for and supporting each other.</p>
+
+      {/* <div className='container px-5 md:px-40 pt-11'>
         <h3 className='pb-6'>About Data Integrated Limited (DIL)</h3>
         <p>Data Integrated Limited (DIL) is a Kenyan ICT company that offers financial solutions to small and medium scale enterprises (SME’s) in Africa. The company humble beginnings dates back to 2012, armed with a big dream of addressing automation challenges faced by small and medium scale businesses (SME’s) in Africa.
           We believe that SME’s would realize their financial goals sooner by deploying modern technology. The efficiency thus gained would translate to growth and longevity of the businesses.</p>
         <p><span className='my-4 font-bold'>Our motto</span>  says a lot on what we believe in, which is connecting “the street” to “high finance”. </p>
         <p>Over the years, DIL has mastered the African market and offers niche solutions that are customized to the unique African experience. We are committed to giving efficient services, seamless customers experience and user friendly person-to-person payments.<br />
           Our solutions are developed to be compatible with most payments solutions and platforms. DIL strives to have financial inclusion for all, by increasing payment efficiency through liable, secure, fast and easy to use devices at an affordable price.</p>
-      </div>
+      </div> */}
 
-      <div className='py-12 my-6 bg-primary'>
+      <div className='py-12 my-6'>
         <div className="container px-5 md:px-40">
-          <h4 className='text-center'>Currently Open Roles</h4>
+          <h1 className='text-center'>Available Positions</h1>
           <div className='flex flex-col py-5 gap-y-5'>
-            {!careers  || careers.length === 0 ? (<div className='p-4 bg-white rounded'><p>No roles available at the moment</p></div>) :(
-             careers.map(career => (
-              <div className='p-5 rounded-lg bg-defaultWhite' key={career._id}>
-                {career.categories && (
-                  <ul>
-                    {career.categories.map(category => <p className='mb-2 font-extrabold uppercase' key={category}>{category}</p>)}
-                  </ul>
-                )}
+            {/* {
+              careers.map(career => (
+                <div className='p-5 rounded-lg bg-defaultWhite' key={career._id}>
+                  {career.categories && (
+                    <ul>
+                      {career.categories.map(category => <p className='mb-2 font-extrabold uppercase' key={category}>{category}</p>)}
+                    </ul>
+                  )}
 
-                <div className="flex items-center justify-between">
-                  <p>{career.title}</p>
-                  <Link href="/careers/[id]" as={`/careers/${career.slug.current}`} passHref ><div className='flex text-[#0645AD] cursor-pointer gap-x-2'>View job<img src="/assets/link.svg" alt="" className="" /></div></Link>
+                  <div className="flex items-center justify-between">
+                    <p>{career.title}</p>
+                    <Link href="/careers/[id]" as={`/careers/${career.slug.current}`} passHref ><div className='flex text-[#0645AD] cursor-pointer gap-x-2'>View job<img src="/assets/link.svg" alt="" className="" /></div></Link>
+                  </div>
+                  <div className="flex">
+                    <p>{career?.location}</p>
+                    <p>{career?.employmentType}</p>
+                  </div>
                 </div>
-                <div className="flex">
-                  <p>{career?.location}</p>
-                  <p>{career?.employmentType}</p>
-                </div>
-              </div>
-            )))}
+              ))} */}
           </div>
           <div className='text-center'>
-            <h4 className='pb-3'>Open Application</h4>
+            <h1 className='pb-3'>Open Application</h1>
             <p>If you don’t see an available position but think you can still be a valuable addition to our team, please send in your application to <span className='font-bold text-blue-500'>hr@dataintegrated.co.ke</span>  and convince us.</p>
-            <h4 className='py-3'>Internships</h4>
+            <h1 className='py-3'>Internships</h1>
             <p>We always welcome interns with high energy, enthusiasm and a keen willingness to learn from day one. Let us know if you are one of them.</p>
           </div>
         </div>
       </div>
 
       <div className='container px-8 py-8 mx-auto text-center md:px-40'>
-        <h4 className=''>Benefits of working for us</h4>
+        <h1 className=''>Benefits of working for us</h1>
         <p className='pt-6'>In addition to contributing to the great cause of easing the lives of millions of Africans with world-class financial products, you also get competitive base salaries, social and statuatory benefits, generous holidays, commissions / bonus / profit sharing schemes based on responsibilities and results of different positions.</p>
       </div>
     </div>
--- src/pages/careers/[slug].tsx
+++ src/pages/careers/[slug].tsx
@@ -3,122 +3,81 @@ import { ICareer } from "src/models/careers";
 const Career = ({ title, location, publishedAt, employmentType }: ICareer) => {
     return (
         <div className="">
-            <div className="container md:pt-16">
-                <h2 className="mb-2 capitalize">{title}</h2>
-                <p>Data Integrated Limited (DIL)</p>
-                <div className="flex items-center justify-between py-10">
-                    <div className="flex flex-1 gap-x-9">
-                        <div className="flex items-center gap-x-1"><img src="/assets/briefcase.svg" alt="" /> <p>{employmentType}</p></div>
-                        <div className="flex items-center gap-x-1"><img src="/assets/calendar.svg" alt="" /> <p><span className="font-extrabold">Apply By: </span>{employmentType}</p></div>
-                    </div>
-                    <button
-                        className="px-5 py-2.5 text-gray-700 border rounded-md duration-100 hover:border-secondary active:shadow-lg"
-                    >
-                        Share Posting
-                    </button>
-                </div>
-            </div>
 
-            <div className="bg-primary">
 
-                <div className="container pt-24">
-                    <div className="flex flex-wrap">
-                        <div className="pr-16 mb-10 md:w-2/3 sm:w-full">
-                            <h3 className="my-2 capitalize">About the role</h3>
-                            <p className="">We are searching for a JavaScript developer that is also familiar with React.js. The majority of your time will be spent designing user interface components and putting them together using well-known React.js procedures. You&apos;ll make sure that these components, as well as the entire application, are reliable and simple to maintain. You&apos;ll collaborate with the rest of the team on various levels of the infrastructure. To satisfy our company&apos;s demands, you should be able to create and manage functioning and stable web apps.</p>
-                            <h3 className="my-2">What you will do:</h3>
-                            <ul className="list-disc list-inside">
-                                <li>Collaborating with engineers, designers, product managers and other stakeholders to actualize new ideas into working consumer features.</li>
-                                <li>Improving application performance by profiling components, debugging data integration and testing across a vast array of web-capable devices and browsers.</li>
-                                <li>Participate in code reviews and working sessions with other engineers to ensure high coding standards are upheld and best practices are considered when structuring code.</li>
-                                <li>Participate in team showcases to showcase the work you and your team have covered and collect feedback.</li>
-                            </ul>
+            <div className="">
 
-                            <h3 className="my-2">What you need to have:</h3>
-                            <ul className="list-disc list-inside">
-                                <li>Experience building web applications</li>
-                                <li>Knowledge of HTML, CSS and SASS preprocessor</li>
-                                <li>Knowledge of JS and Typescript</li>
-                                <li>Experience with git version control</li>
-                                <li>Knowledge of modern authorization mechanisms. </li>
-                                <li>Experience with React</li>
-                            </ul>
+                <div className="pt-24">
+                    <div className="container flex flex-wrap">
+                        <div className="">
+                            <h1>Customer Success Manager</h1>
+                            <p>Division: Sales & Marketing</p>
+                            <p>Experience: 4-7 Years </p>
+                            <p>Location: Nairobi, Kenya or Remote</p>
                         </div>
-                        <div className="p-5 rounded-md bg-defaultWhite md:w-1/3">
-                            <h4>Role Overview</h4>
-                            <div className="flex flex-col pt-3 gap-y-4">
-                                <div>
-                                    <p className="capitalize">Function</p>
-                                    <p>{location}</p>
-                                </div>
-                                <div>
-                                    <p className="capitalize">location</p>
-                                    <p>{location}</p>
-                                </div>
-                                <div>
-                                    <p className="capitalize">Posted On</p>
-                                    <p>{publishedAt.toString()}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        <div className="sm:w-full">
+                            <h3 className="capitalize">About the role</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril</p>
+                            <h3 className="my-10">What you will do:</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril</p>
 
-                    <div className="">
-                        <h3>Apply for this position</h3>
-                        <form
-                            onSubmit={(e) => e.preventDefault()}
-                            className="max-w-lg mt-3">
-                            <div>
-                                <label htmlFor="name" className="block py-3 text-gray-500">
-                                    Your Name*
-                                </label>
-                                <input
-                                    type="text"
-                                    placeholder=""
-                                    id="name"
-                                    className="w-full text-gray-500 outline-none border rounded-md bg-defaultWhite p-2.5"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="email" className="block py-3 text-gray-500">
-                                    Your Email*
-                                </label>
-                                <input
-                                    type="email"
-                                    placeholder=""
-                                    id="email"
-                                    className="w-full text-gray-500 outline-none border rounded-md bg-defaultWhite p-2.5"
-                                />
-                            </div>
-                            <div >
-                                <label htmlFor="website-url" className="block py-2 text-gray-500">
-                                    LinkedIn Profile URL
-                                </label>
-                                <div className="flex items-center text-gray-400 border rounded-md">
-                                    <div className="px-3 py-2.5 rounded-l-md bg-gray-50 border-r">
-                                        https://
-                                    </div>
+                            <h3 className="">What you need to have:</h3>
+                            <p>Euis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea com- modo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui</p>
+                            <h3 className="my-10">What we offer</h3>
+                            <p>Euis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea com- modo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui</p>
+                        </div>
+
+                    </div>
+                    <div className="container py-5">
+                        <button className="text-2xl text-secondary">Apply Now</button>
+                    </div>
+                    <div className="bg-[#F9DADD]">
+                        <div className="container md:pt-10">
+
+                            <form
+                                onSubmit={(e) => e.preventDefault()}
+                                className="max-w-lg mt-3">
+                                <div>
+                                    <label htmlFor="name" className="block py-3 text-gray-500">
+                                        Your Name*
+                                    </label>
                                     <input
                                         type="text"
-                                        placeholder="www.linkedin.com/in/"
-                                        id="website-url"
-                                        className="w-full p-2.5 pl-2 bg-defaultWhite outline-none rounded-r-md"
+                                        placeholder=""
+                                        id="name"
+                                        className="w-full text-gray-500 outline-none border rounded-md bg-defaultWhite p-2.5"
                                     />
                                 </div>
-                            </div>
+                                <div>
+                                    <label htmlFor="email" className="block py-3 text-gray-500">
+                                        Your Email*
+                                    </label>
+                                    <input
+                                        type="email"
+                                        placeholder=""
+                                        id="email"
+                                        className="w-full text-gray-500 outline-none border rounded-md bg-defaultWhite p-2.5"
+                                    />
+                                </div>
+                                <div className="">
+                                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                                    <textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+                                </div>
 
-                            <p className="py-6 text-xs">By submitting this form, you agree that we may use the data you provide to contact you with information related to your submission.
-                                <br />Your data will be used subject to DIL&apos;s Privacy Policy.</p>
-                            <div className="flex items-center justify-between pb-28">
-                                <button
-                                    className="px-5 py-2.5 text-white bg-[#093749] rounded-md duration-150 active:shadow-lg"
-                                >
-                                    Submit Application
-                                </button>
-                                <p>Required Fields *</p>
-                            </div>
 
-                        </form>
+                                <p className="py-6 text-xs">By submitting this form, you agree that we may use the data you provide to contact you with information related to your submission.
+                                    <br />Your data will be used subject to DIL&apos;s Privacy Policy.</p>
+                                <div className="flex items-center justify-between pb-28">
+                                    <button
+                                        className="text-2xl py-2.5 font-SatoshiBold text-secondary"
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+
+                            </form>
+                        </div>
+
                     </div>
 
                 </div>
@@ -131,21 +90,21 @@ export default Career
 
 
 
-export const getServerSideProps = async (pageContext: { query: { slug: string; }; }) => {
-    const pageSlug = pageContext.query.slug;
-    const query = encodeURIComponent(`*[ _type == "career" && slug.current == "${pageSlug}" ]`);
-    const url = `https://2nwbip7f.api.sanity.io/v1/data/query/production?query=${query}`;
-    const result = await fetch(url).then(res => res.json());
-    const career = result.result[0];
-    return {
-        props: {
-            title: career?.title,
-            location: career?.location,
-            publishedAt: new Date(career?.publishedAt).toLocaleDateString(),
-            employmentType: career?.employmentType,
-            category: career?.categories,
-        }
-    }
-};
+// export const getServerSideProps = async (pageContext: { query: { slug: string; }; }) => {
+//     const pageSlug = pageContext.query.slug;
+//     const query = encodeURIComponent(`*[ _type == "career" && slug.current == "${pageSlug}" ]`);
+//     const url = `https://2nwbip7f.api.sanity.io/v1/data/query/production?query=${query}`;
+//     const result = await fetch(url).then(res => res.json());
+//     const career = result.result[0];
+//     return {
+//         props: {
+//             title: career?.title,
+//             location: career?.location,
+//             publishedAt: new Date(career?.publishedAt).toLocaleDateString(),
+//             employmentType: career?.employmentType,
+//             category: career?.categories,
+//         }
+//     }
+// };
 
 
